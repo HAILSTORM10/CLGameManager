b07b8af057a0c6ca4bf977cb6e7cbb7f283f802b
HAILSTORM10 <70339770+HAILSTORM10@users.noreply.github.com>
2026-05-07 22:20:13 +0200

    Switched to Argument-System
    Add
    List
    Start
    
    All Functional

--- Tracker.py
+++ Tracker.py
@@ -1,32 +1,41 @@
-print('Welcome to The Number 1 Game tracker!. What would you like to do? \n 1 - Add game \n 2 - List Games \n Exit - Close program')
-
+#print('Welcome to The Number 1 Game tracker!. What would you like to do? \n 1 - Add game \n 2 - List Games \n Exit - Close program')
+import sys
+import subprocess
 
 arrGames = []
+dictGame = {}
 
 def UserChoice():
-    if Choice == '1':
+    
+    if sChoice.upper() == 'ADD':
+            
             print('You have chosen to add a game.')
             tFile = open('Tracking.txt', 'a')
-            sName = input('Enter name of Game: ')
-            tFile.write(f'{sName}\n')
-            print(f'{sName} has been saved!')
-            #Main()
+            #sName = sys.argv[2]
+            #sExec = sys.argv[3]
+           # sEntry = {'Name':sys.argv[2]} | {'Location':sys.argv[3]}
+            tFile.write(f'{sys.argv[2]} | {sys.argv[3]} \n')
+            print(f'{sys.argv[2]} has been saved!')
             tFile.close()
-    elif Choice == '2':
+    
+    elif sChoice.upper() == 'LIST':
         tFile = open('Tracking.txt', 'r+')
-        for Game in tFile:
-            arrGames.append(Game) 
-        print(arrGames)
+        for GameName in tFile:
+            dictGame = {'Name':GameName.split(' | ')[0], 'Location':GameName.split(' | ')[1]}
+            print(f'{dictGame.get('Name')} - {dictGame.get('Location')}')
         tFile.close()
+    
+    elif sChoice.upper() == 'START':
+        tFile = open('Tracking.txt', 'r+')
+        for GameName in tFile:
+           dictGame = {'Name':GameName.split(' | ')[0], 'Location':GameName.split(' | ')[1]}
+           if sys.argv[2].upper() == dictGame.get('Name').upper() :
+            print('Game found!')
+            subprocess.run(dictGame.get('Location'))   
+        tFile.close
+        
+         
 
-def Main():
-    global Choice 
-    Choice = ''
-    while Choice.upper() != 'EXIT':    
-        Choice = input('What will you be doing: ')
-        UserChoice()
-    else:
-        print('Bye Bye!')
-
-Main()
-#UserChoice(Choice)
+if sys.argv[1] != '':
+    sChoice = sys.argv[1]
+    UserChoice()
